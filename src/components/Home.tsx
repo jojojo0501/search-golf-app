1418bad4ad97c5ce9bc030b9025d5c04185601a2
jojojo0501 <sarukiti0418@outlook.jp>
2023-01-22 10:38:43 +0900

    add list display from data of mock api

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,22 +1,77 @@
-import React from 'react';
+import React, { useState, Key } from 'react';
 import './Common.css';
+import DatePicker, { registerLocale } from 'react-datepicker';
+import ja from 'date-fns/locale/ja';
+import axios from 'axios';
+import addDays from 'date-fns/addDays';
+import { Result } from './Result';
+
+export type Plan = {
+  plan_id: Key;
+  image_url: string;
+  course_name: string;
+  duration: string;
+  price: string;
+  evaluation: string;
+  prefecture: string;
+  plan_name: string;
+  caption: string;
+  reserve_url_pc: string;
+};
 
 export const Home = () => {
+  const today = new Date();
+  const [date, setDate] = useState<Date>(today);
+  const [budget, setBudget] = useState<number>(8000);
+  const [departure, setDeparture] = useState<number>(1);
+  const [duration, setDuration] = useState<number>(60);
+  const [plans, setPlans] = useState<Plan[]>([]);
+  registerLocale('ja', ja);
+
+  const onFormSubmit = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    const response = await axios.get(
+      'https://l1kwik11ne.execute-api.ap-northeast-1.amazonaws.com/production/golf-courses',
+      {
+        params: {
+          date: addDays(date, 14),
+          budget,
+          departure,
+          duration,
+        },
+      }
+    );
+    setPlans(response.data.plans);
+  };
+
   return (
     <div className="ui container" id="container">
       <div className="Search__Form">
-        <form className="ui form segment">
+        <form className="ui form segment" onSubmit={onFormSubmit}>
           <div className="field">
             <label>
               <i className="calendar alternate outline icon"></i>プレー日
             </label>
-            <input type="date" />
+            <DatePicker
+              dateFormat="yyyy/MM/dd"
+              locale="ja"
+              selected={date}
+              minDate={today}
+              onChange={(selectedDate) => {
+                setDate(selectedDate || today);
+              }}
+            />
           </div>
           <div className="field">
             <label>
               <i className="yen sign icon"></i>上限金額
             </label>
-            <select className="ui dropdown" name="dropdown">
+            <select
+              className="ui dropdown"
+              name="dropdown"
+              value={budget}
+              onChange={(e) => setBudget(Number(e.target.value))}
+            >
               <option value="8000">8,000円</option>
               <option value="12000">12,000円</option>
               <option value="16000">16,000円</option>
@@ -27,7 +82,12 @@ export const Home = () => {
               <i className="map pin icon"></i>
               移動時間計算の出発地点（自宅から近い地点をお選びください）
             </label>
-            <select className="ui dropdown" name="dropdown">
+            <select
+              className="ui dropdown"
+              name="dropdown"
+              value={departure}
+              onChange={(e) => setDeparture(Number(e.target.value))}
+            >
               <option value="1">東京駅</option>
               <option value="2">横浜駅</option>
             </select>
@@ -36,7 +96,12 @@ export const Home = () => {
             <label>
               <i className="car icon"></i>車での移動時間の上限
             </label>
-            <select className="ui dropdown" name="dropdown">
+            <select
+              className="ui dropdown"
+              name="dropdown"
+              value={duration}
+              onChange={(e) => setDuration(Number(e.target.value))}
+            >
               <option value="60">60分</option>
               <option value="90">90分</option>
               <option value="120">120分</option>
@@ -48,6 +113,7 @@ export const Home = () => {
             </button>
           </div>
         </form>
+        <Result plans={plans} />
       </div>
     </div>
   );
